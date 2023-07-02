7c23caaab001b60cd0f1ac6c0598dd0d84e8bf15
Andrew Smith <meuzishun@gmail.com>
2023-07-01 21:08:21 -0400

    Add auth header to postsLoader

--- src/loaders/postsLoader.js
+++ src/loaders/postsLoader.js
@@ -1,7 +1,11 @@
 const apiRoot = import.meta.env.VITE_API_ROOT;
 
 export const postsLoader = async () => {
-  const res = await fetch(apiRoot + '/posts');
+  const res = await fetch(apiRoot + '/posts', {
+    headers: {
+      Authorization: localStorage.getItem('token'),
+    },
+  });
 
   if (!res.ok) {
     throw Error('Could not fetch posts');
