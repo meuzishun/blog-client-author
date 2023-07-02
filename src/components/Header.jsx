534d2dfd34ee0f867b7095cd3c5428c27bc1324e
Andrew Smith <meuzishun@gmail.com>
2023-07-01 21:39:10 -0400

    Add logout button to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
     <header>
       <h1>You&apos;re Gonna Make Me Blog</h1>
@@ -10,6 +12,15 @@ export default function Header() {
         <Link to='posts'>Posts</Link>
         <br />
         <Link to='posts/new'>New Post</Link>
+        <br />
+        <button
+          onClick={() => {
+            localStorage.clear();
+            navigate('/login');
+          }}
+        >
+          Logout
+        </button>
       </nav>
       <hr />
     </header>
