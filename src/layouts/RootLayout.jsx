c453f5b46f18834a53fb094b75e564fa60b07907
Andrew Smith <meuzishun@gmail.com>
2023-07-01 22:17:17 -0400

    Simplify RootLayout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,20 +1,7 @@
+import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
-import { useContext, useEffect } from 'react';
-import { useNavigate, Outlet } from 'react-router-dom';
-import { UserContext } from '../App';
 
 export default function RootLayout() {
-  const { user } = useContext(UserContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user) {
-      navigate('/login');
-    } else {
-      navigate('/');
-    }
-  }, [user]);
-
   return (
     <div>
       <Header />
