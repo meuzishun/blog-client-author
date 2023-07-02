62f90271fd12ec3de077b1477d542ecfa33cecfb
Andrew Smith <meuzishun@gmail.com>
2023-07-01 21:41:27 -0400

    Create a userLoader file for auth

---
+++ src/loaders/userLoader.js
@@ -0,0 +1,28 @@
+const apiRoot = import.meta.env.VITE_API_ROOT;
+
+export const userLoader = async () => {
+  const token = localStorage.getItem('token');
+
+  if (!token) {
+    localStorage.clear();
+    return false;
+  }
+
+  const response = await fetch(apiRoot + '/profile', {
+    method: 'GET',
+    headers: {
+      Authorization: token,
+    },
+  });
+
+  const data = await response.json();
+
+  if (!data.user || !data.user.isAdmin) {
+    localStorage.clear();
+    return false;
+  }
+
+  const userString = JSON.stringify(data.user);
+  localStorage.setItem('user', userString);
+  return data.user;
+};
