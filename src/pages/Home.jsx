a0e5adcd5a0374e5b877e3e99f3d7aa20c5139be
Andrew Smith <meuzishun@gmail.com>
2023-07-03 13:04:35 -0400

    Create index.scss file and test a few styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>
+    <div className='test white-text'>
       <h2>Homepage</h2>
       <br />
       <Link to='posts'>View Posts</Link>
