065d083e1efc5fcc6b20e78b286216284124411e
Andrew Smith <meuzishun@gmail.com>
2023-07-03 19:34:24 -0400

    Style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,23 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <h2>Homepage</h2>
-      <br />
-      <Link to='posts'>View Posts</Link>
-      <br />
-      <Link to='posts/new'>Create a New Post</Link>
+      <h2 className='mt-3 mb-3 pl-2 pr-2 display-f justify-center text-primary font-xl'>
+        Welcome to You&apos;re Gonna Make Me Blog
+      </h2>
+      <div className='display-f justify-center wrap'>
+        <Link
+          className='btn-outlined-secondary text-secondary text-hover-white font-lg m-2'
+          to='posts'
+        >
+          View Posts
+        </Link>
+        <Link
+          className='btn-outlined-secondary text-secondary text-hover-white font-lg m-2'
+          to='posts/new'
+        >
+          Create a New Post
+        </Link>
+      </div>
     </div>
   );
 }
