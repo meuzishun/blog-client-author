526f31a186cbc2de861bfdad19b2999299d617c6
Andrew Smith <meuzishun@gmail.com>
2023-07-03 16:39:29 -0400

    Copy over shinobi style library and remove test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div className='test white-text'>
+    <div>
       <h2>Homepage</h2>
       <br />
       <Link to='posts'>View Posts</Link>
