3e0a5ae7ed86bf432911267941e53eaeba0c99a8
Andrew Smith <meuzishun@gmail.com>
2023-07-04 19:42:23 -0400

    Style Posts.jsx

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -29,30 +29,30 @@ export default function Posts() {
       <div className='container'>
         {posts.map((post) => (
           <div
-            className='row pt-1 pb-1 display-f justify-space-between'
+            className='row pt-1 pb-1 display-f justify-space-between container bg-primary-light-8 br-xs mb-1'
             key={post._id}
           >
             <Link
-              className='font-lg text-hover-primary'
+              className='font-lg mt-1 mb-1 text-hover-primary col-7-xl col-6-lg col-5-md col-12-sm col-12-xs'
               to={post._id.toString()}
             >
               {post.title}
             </Link>
-            <div>
+            <div className='col-4-xl col-5-lg col-6-md col-12-sm col-12-xs justify-flex-end mt-1 mb-1'>
               <Link
-                className='btn-outlined-primary ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-primary text-hover-white'
+                className='btn-outlined-primary ml-1 mr-1 mb-1 pr-1 pl-1 pt-0 pb-0 font-md text-primary text-hover-white'
                 to={post._id.toString() + '/edit'}
               >
                 Edit
               </Link>
               <Link
-                className='btn-outlined-error ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-error text-hover-white'
+                className='btn-outlined-error ml-1 mr-1 mb-1 pr-1 pl-1 pt-0 pb-0 font-md text-error text-hover-white'
                 to={post._id.toString() + '/delete'}
               >
                 Delete
               </Link>
               <button
-                className='btn-outlined-secondary ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-secondary text-hover-white pub-btn'
+                className='btn-outlined-secondary ml-1 mr-1 mb-1 pr-1 pl-1 pt-0 pb-0 font-md text-secondary text-hover-white pub-btn'
                 onClick={() => {
                   togglePublication(post);
                 }}
@@ -62,7 +62,7 @@ export default function Posts() {
             </div>
           </div>
         ))}
-        <div className='row justify-center mt-3'>
+        <div className='row justify-center mt-3 mb-3'>
           <Link className='btn-primary font-lg text-white' to='new'>
             Create New Post
           </Link>
