d25ebbe6dbb3f582d13e4691b9a05d92f083757c
Andrew Smith <meuzishun@gmail.com>
2023-07-04 08:58:21 -0400

    Style Posts page

--- src/pages/posts/Posts.jsx
+++ src/pages/posts/Posts.jsx
@@ -23,25 +23,48 @@ export default function Posts() {
 
   return (
     <div>
-      {posts.map((post) => (
-        <div key={post._id}>
-          <Link to={post._id.toString()}>{post.title}</Link>
-          {' | '}
-          <Link to={post._id.toString() + '/edit'}>Edit</Link>
-          {' | '}
-          <Link to={post._id.toString() + '/delete'}>Delete</Link>
-          {' | '}
-          <button
-            onClick={() => {
-              togglePublication(post);
-            }}
+      <h3 className='mt-3 mb-1 pl-2 pr-2 display-f justify-center text-primary font-xl'>
+        Posts
+      </h3>
+      <div className='container'>
+        {posts.map((post) => (
+          <div
+            className='row pt-1 pb-1 display-f justify-space-between'
+            key={post._id}
           >
-            {post.isPublished ? 'Unpublish' : 'Publish'}
-          </button>
-          <hr />
+            <Link className='font-lg' to={post._id.toString()}>
+              {post.title}
+            </Link>
+            <div>
+              <Link
+                className='btn-outlined-primary ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-primary text-hover-white'
+                to={post._id.toString() + '/edit'}
+              >
+                Edit
+              </Link>
+              <Link
+                className='btn-outlined-error ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-error text-hover-white'
+                to={post._id.toString() + '/delete'}
+              >
+                Delete
+              </Link>
+              <button
+                className='btn-outlined-secondary ml-1 mr-1 pr-1 pl-1 pt-0 pb-0 font-md text-secondary text-hover-white pub-btn'
+                onClick={() => {
+                  togglePublication(post);
+                }}
+              >
+                {post.isPublished ? 'Unpublish' : 'Publish'}
+              </button>
+            </div>
+          </div>
+        ))}
+        <div className='row justify-center mt-3'>
+          <Link className='btn-primary font-lg text-white' to='new'>
+            Create New Post
+          </Link>
         </div>
-      ))}
-      <Link to='new'>Create New Post</Link>
+      </div>
     </div>
   );
 }
