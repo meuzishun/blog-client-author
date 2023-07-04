975c2c591661c43e1abac136f8b715c7de2c75cb
Andrew Smith <meuzishun@gmail.com>
2023-07-04 16:09:13 -0400

    Style create comment and restyle create post

--- src/pages/comments/CreateComment.jsx
+++ src/pages/comments/CreateComment.jsx
@@ -1,26 +1,50 @@
+import { useRef, useEffect } from 'react';
 import { Form, useNavigate, useParams } from 'react-router-dom';
 
 export default function CreateComment() {
   const navigate = useNavigate();
   const { postId } = useParams();
+  const contentInput = useRef(null);
+
+  useEffect(() => {
+    contentInput.current.focus();
+  }, []);
 
   return (
-    <div>
-      <h3>Create a new comment</h3>
-      <Form method='post' action={'/posts/' + postId + '/comments/new'}>
-        <div>
-          <label>Content</label>
-          <textarea name='content' id='content' required></textarea>
+    <div className='container p-0'>
+      <h3 className='row justify-center mt-3 font-lg text-primary'>
+        Create a new comment
+      </h3>
+      <Form
+        className='row justify-center'
+        method='post'
+        action={'/posts/' + postId + '/comments/new'}
+      >
+        <div className='col-12-xs col-9-md col-7-lg container'>
+          <label className='row col-12-xs col-9-md col-7-lg'>Content</label>
+          <textarea
+            className='row col-12-xs font-md p-1'
+            name='content'
+            id='content'
+            rows='2'
+            required
+            ref={contentInput}
+          ></textarea>
         </div>
-        <button type='submit'>Submit</button>
-        <button
-          type='button'
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Cancel
-        </button>
+        <div className='mt-3 mb-3 col-12-xs col-9-md col-7-lg display-f justify-center'>
+          <button className='btn-secondary text-white ml-1 mr-1' type='submit'>
+            Submit
+          </button>
+          <button
+            className='btn-gray text-white ml-1 mr-1'
+            type='button'
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
       </Form>
     </div>
   );
--- src/pages/posts/CreatePost.jsx
+++ src/pages/posts/CreatePost.jsx
@@ -1,33 +1,64 @@
+import { useRef, useEffect } from 'react';
 import { Form, useNavigate } from 'react-router-dom';
 
 export default function CreatePost() {
   const navigate = useNavigate();
+  const titleInput = useRef(null);
+
+  useEffect(() => {
+    titleInput.current.focus();
+  }, []);
 
   return (
-    <div>
-      <h3>Create a new post</h3>
-      <Form method='post' action='/posts/new'>
-        <div>
-          <label>Title</label>
-          <input type='text' name='title' id='title' required></input>
+    <div className='container'>
+      <h3 className='row justify-center mt-3 font-lg text-primary'>
+        Create a new post
+      </h3>
+      <Form className='row justify-center' method='post' action='/posts/new'>
+        <div className='col-12-xs col-9-md col-7-lg'>
+          <label className='row mt-2'>Title</label>
+          <input
+            ref={titleInput}
+            className='row container font-md p-1'
+            type='text'
+            name='title'
+            id='title'
+            required
+          ></input>
         </div>
-        <div>
-          <label>Content</label>
-          <textarea name='content' id='content' required></textarea>
+        <div className='col-12-xs col-9-md col-7-lg'>
+          <label className='row mt-2'>Content</label>
+          <textarea
+            className='row container font-md p-1'
+            name='content'
+            id='content'
+            rows='10'
+            required
+          ></textarea>
         </div>
-        <div>
-          <label>Publish</label>
+        <div className='col-12-xs col-9-md col-7-lg'>
+          <label className='row mt-2' htmlFor='publish'>
+            Publish
+          </label>
           <input name='publish' id='publish' type='checkbox' />
         </div>
-        <button type='submit'>Submit</button>
-        <button
-          type='button'
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Cancel
-        </button>
+        <div className='container display-f justify-center mt-3'>
+          <button
+            className='btn-primary font-md text-white ml-1 mr-1'
+            type='submit'
+          >
+            Submit
+          </button>
+          <button
+            className='btn-gray font-md text-white ml-1 mr-1'
+            type='button'
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
       </Form>
     </div>
   );
