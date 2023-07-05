937e89b7177b62841abf17c5ac8cdee169c475e9
Andrew Smith <meuzishun@gmail.com>
2023-07-05 03:48:13 -0400

    Style EditComment page

--- src/pages/comments/EditComment.jsx
+++ src/pages/comments/EditComment.jsx
@@ -6,34 +6,39 @@ export default function EditComment() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h3>
-        Edit comment {commentId} in post {postId}
+    <div className='container p-0'>
+      <h3 className='row justify-center mt-3 font-lg text-primary'>
+        Edit comment
       </h3>
       <Form
+        className='row justify-center'
         method='post'
         action={'/posts/' + postId + '/comments/' + commentId + '/edit'}
       >
-        <p>
-          {comment.author.firstName} {comment.author.lastName}
-        </p>
-        <div>
-          <label>Content</label>
+        <div className='col-12-xs col-9-md col-7-lg container'>
+          <label className='row col-12-xs col-9-md col-7-lg'>Content</label>
           <textarea
+            className='row col-12-xs font-md p-1'
             id='content'
             name='content'
+            rows='2'
             defaultValue={comment.content}
           ></textarea>
         </div>
-        <button type='submit'>Submit</button>
-        <button
-          type='button'
-          onClick={() => {
-            navigate(-1);
-          }}
-        >
-          Cancel
-        </button>
+        <div className='mt-3 mb-3 col-12-xs col-9-md col-7-lg display-f justify-center'>
+          <button className='btn-secondary text-white ml-1 mr-1' type='submit'>
+            Submit
+          </button>
+          <button
+            className='btn-gray text-white ml-1 mr-1'
+            type='button'
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
       </Form>
     </div>
   );
