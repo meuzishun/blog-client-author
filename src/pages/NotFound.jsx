911c30af8e724b429d98525333b221818a7b8d62
Andrew Smith <meuzishun@gmail.com>
2023-07-05 13:35:52 -0400

    Style NotFound page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,11 +2,19 @@ import { Link } from 'react-router-dom';
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Oops! Page not found!</h2>
-      <p>Looks like there is no page at this url...</p>
-      <p>
-        Go to the <Link to='/'>Homepage</Link>.
+    <div className='container'>
+      <h2 className='row justify-center mt-3 text-primary font-xl'>
+        Oops! Page not found!
+      </h2>
+      <p className='row justify-center mt-3 font-lg'>
+        Looks like there is no page at this url...
+      </p>
+      <p className='row justify-center mt-3 font-lg'>
+        Go to the&nbsp;
+        <Link className='text-primary text-hover-secondary' to='/'>
+          Homepage
+        </Link>
+        .
       </p>
     </div>
   );
