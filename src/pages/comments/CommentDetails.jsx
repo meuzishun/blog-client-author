c3bbf3e07c18b249fea56d78e24fee9266ed9056
Andrew Smith <meuzishun@gmail.com>
2023-07-05 05:22:13 -0400

    Style individual comment page

--- src/pages/comments/CommentDetails.jsx
+++ src/pages/comments/CommentDetails.jsx
@@ -5,15 +5,27 @@ export default function CommentDetails() {
 
   return (
     <div>
-      <h3>Details for post {comment._id}</h3>
-      <p>
-        {comment.author.firstName} {comment.author.lastName}
+      <h3 className='row justify-center mt-3 ml-1 mr-1 font-lg text-primary'>
+        Comment Details
+      </h3>
+      <p className='row justify-center mt-1 ml-1 mr-1 font-md text-primary'>
+        {comment.author.firstName} {comment.author.lastName} - &quot;
+        {comment.content}&quot;
       </p>
-      <p>{comment.content}</p>
-      <br />
-      <Link to='edit'>Edit</Link>
-      <br />
-      <Link to='delete'>Delete Comment</Link>
+      <div className='display-f justify-center mt-3 mb-3'>
+        <Link
+          className='btn-outlined-gray text-gray text-hover-white font-md ml-1 mr-1'
+          to='edit'
+        >
+          Edit Comment
+        </Link>
+        <Link
+          className='btn-outlined-red text-red text-hover-white font-md ml-1 mr-1'
+          to='delete'
+        >
+          Delete Comment
+        </Link>
+      </div>
     </div>
   );
 }
